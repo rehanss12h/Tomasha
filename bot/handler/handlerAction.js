819dd03cd0df0afbc210c7043ce5da40876803a4
SADIK x69 <sadikbot486@gmail.com>
2025-12-02 15:21:43 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -1,97 +1,104 @@
 const createFuncMessage = global.utils.message;
 const handlerCheckDB = require("./handlerCheckData.js");
-const fs = require("fs");
+
+const request = require("request")
+const axios = require("axios")
+const fs = require("fs-extra")
+
 
 module.exports = (api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData) => {
-	const handlerEvents = require(
-		process.env.NODE_ENV == "development"
-			? "./handlerEvents.dev.js"
-			: "./handlerEvents.js"
-	)(api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData);
+	const handlerEvents = require(process.env.NODE_ENV == 'development' ? "./handlerEvents.dev.js" : "./handlerEvents.js")(api, threadModel, userModel, dashBoardModel, globalModel, usersData, threadsData, dashBoardData, globalData);
 
 	return async function (event) {
-		// 🔒 Anti-inbox check
-		if (
-			global.GoatBot.config.antiInbox == true &&
-			(event.senderID == event.threadID ||
-				event.userID == event.senderID ||
-				event.isGroup == false) &&
-			(event.senderID || event.userID || event.isGroup == false)
-		) return;
-
-		// 🚫 Global ban check for threads
-		const banPath = __dirname + "/cmds/cache/thread-manage.json";
-		if (fs.existsSync(banPath)) {
-			const banData = JSON.parse(fs.readFileSync(banPath));
-			const isBanned = banData.banList.some(t => t.id === event.threadID);
-			if (isBanned) {
-				if (["message", "message_reply", "message_reaction", "event"].includes(event.type)) {
-					return api.sendMessage("🚫 This group is *banned* from using the bot!", event.threadID);
-				}
-				return; // silently block others
-			}
-		}
-
-		// 📨 Create message utils
 		const message = createFuncMessage(api, event);
 
-		// 🔍 DB check
 		await handlerCheckDB(usersData, threadsData, event);
-
-		// 🎯 Load event handlers
 		const handlerChat = await handlerEvents(event, message);
-		if (!handlerChat) return;
+		if (!handlerChat)
+			return;
 
-		const {
-			onAnyEvent, onFirstChat, onStart, onChat,
-			onReply, onEvent, handlerEvent, onReaction,
-			typ, presence, read_receipt
-		} = handlerChat;
+		const { onStart, onChat, onReply, onEvent, handlerEvent, onReaction, typ, presence, read_receipt } = handlerChat;
 
-		onAnyEvent();
-
-		// ⚡ Main event switch
 		switch (event.type) {
 			case "message":
 			case "message_reply":
 			case "message_unsend":
-				onFirstChat();
 				onChat();
 				onStart();
 				onReply();
-				break;
+        if(event.type == "message_unsend"){
+          
+          let resend = await threadsData.get(event.threadID, "settings.reSend");
+		if (resend == true && event.senderID 
+!== api.getCurrentUserID()){
+      let umid = global.reSend[event.threadID].findIndex(e => e.messageID === event.messageID)
+      
+      if(umid>(-1)){
+let nname = await usersData.getName(event.senderID)
+        let attch = []
+if(global.reSend[event.threadID][umid].attachments.length>0){
+  let cn = 0
+  for(var abc of global.reSend[event.threadID][umid].attachments){
+   if(abc.type == "audio"){
+    
+    cn += 1;
 
+   let pts = `scripts/cmds/tmp/${cn}.mp3`
+					let res2 = (await axios.get(abc.url, {
+						responseType: "arraybuffer"
+					})).data;
+			fs.writeFileSync(pts, Buffer.from(res2, "utf-8"))
+    
+  attch.push(fs.createReadStream(pts))} else{
+     attch.push(await global.utils.getStreamFromURL(abc.url))
+  }
+  }
+}
+        
+  api.sendMessage({body: nname + " removed:\n\n" + global.reSend[event.threadID][umid].body,
+mentions:[{id:event.senderID, tag:nname}],
+    attachment:attch
+                  }, event.threadID)
+                   
+
+  
+      }
+    }
+        }
+				break;
 			case "event":
 				handlerEvent();
 				onEvent();
 				break;
-
 			case "message_reaction":
-				// ✅ Custom unsend logic (Everyone can use)
-				const allowedReactions = ["🚮", "😠", "😡", "🤬"]; // ইমোজি যেগুলোতে unsend হবে
-
-				// যদি বট নিজে রিঅ্যাক্ট করা মেসেজে allowed emoji থাকে → unsend করবে
-				if (allowedReactions.includes(event.reaction)) {
-					if (event.senderID === api.getCurrentUserID()) {
-						api.unsendMessage(event.messageID);
-					}
-				}
-
 				onReaction();
-				break;
+        if(event.reaction == ""){
+  if(event.userID == "100033670741301","61571904047861"){
+api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
+                if (err) return console.log(err);
+              });
 
+}else{
+    message.send(":)")
+  }
+  }
+        if(event.reaction ==  "🫠"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61580068018277","100050087741692"){
+    message.unsend(event.messageID)
+}else{
+    message.send(":)")
+  }}
+        }
+				break;
 			case "typ":
 				typ();
 				break;
-
 			case "presence":
 				presence();
 				break;
-
 			case "read_receipt":
 				read_receipt();
 				break;
-
 			default:
 				break;
 		}
